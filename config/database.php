84906b8d2db7172833c8ba5596fa20f7aa83f29a
Fatima-EzzahraAboutaleb <fatimaezzahraaboutaleb5gmail.com>
2026-05-05 23:15:44 +0100

    Upadate Config

--- config/database.php
+++ config/database.php
@@ -1,11 +1,12 @@
 <?php
-$db_host = getenv('DB_HOST');
-$db_user = getenv('DB_USER');
-$db_password = getenv('DB_PASS');
-$db_name = getenv('DB_NAME');
+$db_host = getenv('DB_HOST') ?: 'localhost';
+$db_user = getenv('DB_USER') ?: 'root';
+$db_password = getenv('DB_PASS') ?: '';
+$db_name = getenv('DB_NAME') ?: 'clinic_management';
+$db_port = getenv('DB_PORT') ?: 3306;
 
 mysqli_report(MYSQLI_REPORT_OFF);
-$conn = new mysqli($db_host, $db_user, $db_password, $db_name, 3306);
+$conn = new mysqli($db_host, $db_user, $db_password, $db_name, $db_port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
