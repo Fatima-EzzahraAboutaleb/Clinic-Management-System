a0b9e2f1daf299ce841f500990d63a40731ce1e8
Fatima-EzzahraAboutaleb <fatimaezzahraaboutaleb5gmail.com>
2026-05-05 22:20:06 +0100

    Update database config

--- config/database.php
+++ config/database.php
@@ -1,17 +1,15 @@
 <?php
-// Database Configuration
 $db_host = getenv('DB_HOST');
 $db_user = getenv('DB_USER');
 $db_password = getenv('DB_PASS');
 $db_name = getenv('DB_NAME');
-$conn = new mysqli($db_host, $db_user, $db_password, $db_name);
 
-// Check connection
+mysqli_report(MYSQLI_REPORT_OFF);
+$conn = new mysqli($db_host, $db_user, $db_password, $db_name, 3306);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Set charset to utf8
 $conn->set_charset("utf8");
-
 ?>
